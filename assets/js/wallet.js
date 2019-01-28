0b460185777d9ea9f7b12b2eb4239bbc46f8782b
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-01-27 21:18:06 -0800

    Update wallet.js

--- assets/js/wallet.js
+++ assets/js/wallet.js
@@ -205,7 +205,7 @@ function genwallet_prefix()
     prefix_widget = document.getElementById("prefix_widget");
     prefix = prefix_widget.value;
     prefix.trim();
-    if (prefix.length < 5 || prefix[0] != "TRTL") {
+    if (prefix.length < 4 || prefix[0] != "Rhod") {
       alert("Bad prefix "+prefix+" should start with K and be at least one extra character");
       return;
     }
